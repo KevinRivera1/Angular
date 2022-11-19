9c18480620e4499251dccf1eb72e40082de4b96a
Kevin Rivera <shimpo11@outlook.com>
2022-11-19 09:47:50 -0500

    Maquetado HTML dashboard
    - Se agrego nuevos componentes 
    - Diseño de dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { HomeComponent } from './pages/home/home.component';
-import { FooterComponent } from './static/footer/footer.component';
+import { UserComponent } from './user/user/user.component';
 import { HeaderComponent } from './static/header/header.component';
 import { NavComponent } from './static/nav/nav.component';
-import { RegisterComponent } from './user/register/register.component';
-import { UserComponent } from './user/user/user.component';
+import { FooterComponent } from './static/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+import { LoginComponent } from './auth/login/login.component';
+import { BreadcrumbsComponent } from './static/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './static/sidebar/sidebar.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     AboutComponent,
     ContactComponent,
-    HomeComponent,
-    FooterComponent,
+    UserComponent,
+    RegisterComponent,
     HeaderComponent,
     NavComponent,
-    RegisterComponent,
-    UserComponent
+    FooterComponent,
+    DashboardComponent,
+    LoginComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    NotFoundComponent,
+
   ],
   imports: [
     BrowserModule
